db5680389adc0477f6b8385c318710f0c1e972c3
1qawsej1 <jarrettnovel@gmail.com>
2025-09-17 19:14:39 +0800

    Update and rename app.js to meteorData.js

---
+++ meteorData.js
@@ -0,0 +1,13 @@
+export const meteorTemplates = [
+  { name:"Chelyabinsk", size:20, density:3300, material:"stony", velocity:19000 },
+  { name:"Tunguska", size:60, density:3000, material:"stony", velocity:15000 },
+  { name:"Apophis", size:325, density:2500, material:"stony", velocity:12000 },
+  { name:"Benešov", size:100, density:3300, material:"iron", velocity:17000 },
+  { name:"Gibeon", size:80, density:7800, material:"iron", velocity:15000 },
+  { name:"Allende", size:50, density:3500, material:"carbonaceous", velocity:14000 },
+  { name:"Hoba", size:250, density:8000, material:"iron", velocity:13000 },
+  { name:"Orgueil", size:30, density:3500, material:"carbonaceous", velocity:16000 },
+  { name:"Murchison", size:35, density:3200, material:"stony", velocity:15000 },
+  { name:"Sikhote-Alin", size:70, density:7800, material:"iron", velocity:17000 },
+];
+
